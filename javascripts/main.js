bac2bbf62038e3ad3a8aa631d7734cbfa30ef0ef
Aaron Groch <ar.groch@gmail.com>
2020-12-08 12:19:06 -0800

    added new pages and content

---
+++ javascripts/main.js
@@ -0,0 +1,11 @@
+// $.getScript('bootstrap/bootstrap.min.js', function()
+// {
+//     // script is now loaded and executed.
+//     // put your dependent JS here.
+// });
+
+$(document).ready(function(){
+    $("#pictures").click(function(){
+      alert("Pictures was clicked!");
+    });
+});
